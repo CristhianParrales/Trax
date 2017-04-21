da2760505c4bb9731d42e822e117148f77682d99
Cristhian Parrales <cparralt@hotmail.com>
2017-04-20 21:33:41 -0500

    Initial Commit

---
+++ Trax/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trax
+//
+//  Created by Cristhian Parrales on 20/4/17.
+//  Copyright © 2017 Cristhian Parrales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
